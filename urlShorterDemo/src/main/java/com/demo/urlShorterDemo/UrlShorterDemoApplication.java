723f6a21e161ae1606720712e2643a0e6110ca77
PratikshaGShete <120096651+PratikshaGShete@users.noreply.github.com>
2022-12-08 15:50:54 +0530

    Add files via upload

---
+++ urlShorterDemo/src/main/java/com/demo/urlShorterDemo/UrlShorterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.urlShorterDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorterDemoApplication.class, args);
+	}
+
+}
---
+++ urlShorterDemo/src/test/java/com/demo/urlShorterDemo/UrlShorterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.urlShorterDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
